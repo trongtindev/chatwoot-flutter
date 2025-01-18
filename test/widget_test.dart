600d92714776fa6dd2cd7aaee898fb950a565d0a
Tin Nguyen Trong <me@trongtin.dev>
2025-01-19 02:18:55 +0700

    chore: add dummy test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:chatwoot/main.dart';
+// import 'package:chatwoot/main.dart';
 
 void main() {
   // TODO: generate tests
+
+  test('Dummy', () async {
+    expect(1, 1);
+  });
 }
