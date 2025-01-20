9d6c89df6d48fb95fb127e36beb7c9ef2eec6c5c
Tin Nguyen Trong <me@trongtin.dev>
2025-01-20 17:30:23 +0700

    chore: add permission request

---
+++ lib/utils/permision.dart
@@ -0,0 +1,71 @@
+import 'package:permission_handler/permission_handler.dart';
+import 'package:open_settings/open_settings.dart';
+import '/imports.dart';
+
+Future<bool> ensurePermissionsGranted({
+  required List<Permission> permissions,
+}) async {
+  try {
+    for (var permission in permissions) {
+      var status = await permission.status;
+
+      if (status == PermissionStatus.granted) {
+        continue;
+      } else if (status == PermissionStatus.permanentlyDenied) {
+        await Get.dialog(AlertDialog(
+          title: Text(t.permission_denied),
+          content: Text(t.permission_denied_message(permission.toString())),
+          actions: [
+            TextButton(
+              onPressed: () => Get.back(),
+              child: Text(t.close),
+            ),
+            TextButton(
+              onPressed: () {
+                Get.back();
+
+                switch (permission) {
+                  // TODO: handle
+                  default:
+                    OpenSettings.openAppSetting();
+                    break;
+                }
+              },
+              child: Text(t.open_settings),
+            )
+          ],
+        ));
+        return false;
+      }
+
+      await Get.dialog(AlertDialog(
+        title: Text(t.permission_request),
+        content: Text(t.permission_request_message(permission.toString())),
+        actions: [
+          TextButton(
+            onPressed: () => Get.back(),
+            child: Text(t.close),
+          )
+        ],
+      ));
+
+      var result = await permission.request();
+      if (result != PermissionStatus.granted) return false;
+    }
+
+    return true;
+  } on Error catch (error) {
+    logger.e(error, stackTrace: error.stackTrace);
+    Get.dialog(AlertDialog(
+      title: Text(t.exception),
+      content: Text(t.exception_message(error.toString())),
+      actions: [
+        TextButton(
+          onPressed: () => Get.back(),
+          child: Text(t.close),
+        )
+      ],
+    ));
+    return false;
+  }
+}
