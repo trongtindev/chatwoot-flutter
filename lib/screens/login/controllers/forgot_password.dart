749fe7144750b005be85e58e57dbc343182a601e
Tin Nguyen Trong <me@trongtin.dev>
2025-01-22 10:58:15 +0700

    feat: add forgot_password

---
+++ lib/screens/login/controllers/forgot_password.dart
@@ -0,0 +1,41 @@
+import '/imports.dart';
+
+class ForgotPasswordController extends GetxController {
+  final _api = Get.find<ApiService>();
+  final _logger = Logger();
+
+  final formKey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final isLoading = false.obs;
+
+  Future<void> submit() async {
+    if (!formKey.currentState!.validate()) return;
+    isLoading.value = true;
+    _logger.i('submit()');
+
+    try {
+      final result = await _api.resetPassword(
+        email: email.text,
+      );
+      final message = result.getOrThrow();
+
+      Get.dialog(
+        AlertDialog(
+          title: Text(t.successful),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Get.back(),
+              child: Text(t.understand),
+            )
+          ],
+        ),
+      );
+    } catch (error) {
+      if (error is! ApiError) _logger.e(error);
+      errorHandler(error);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
--- lib/screens/login/views/forgot_password.dart
+++ lib/screens/login/views/forgot_password.dart
@@ -1,29 +1,67 @@
-import '../controllers/index.dart';
+import '../controllers/forgot_password.dart';
 import '../widgets/layout.dart';
 import '/imports.dart';
 
-class ForgotPasswordView extends GetView<LoginController> {
-  const ForgotPasswordView({super.key});
+class ForgotPasswordView extends GetView<ForgotPasswordController> {
+  final api = Get.find<ApiService>();
+  final ForgotPasswordController c;
+
+  ForgotPasswordView({super.key}) : c = Get.put(ForgotPasswordController());
 
   @override
   Widget build(BuildContext context) {
-    var width = double.infinity; // TODO: responsive
+    final width = double.infinity; // TODO: responsive
+    final baseUrl = Uri.parse(api.baseUrl.value).host;
 
-    return GetBuilder(
-      init: LoginController(),
-      builder: (_) {
-        return Scaffold(
-          appBar: AppBar(),
-          body: buildBody(
-            width: width,
-            header: buildHeader(
-              title: t.forgot_password_title,
-              description: t.forgot_password_description,
-            ),
-            child: Text('ok'),
-          ),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(),
+      body: buildBody(
+        width: width,
+        header: buildHeader(
+          title: t.forgot_password_title,
+          description: t.forgot_password_description(baseUrl),
+        ),
+        child: buildForm(),
+      ),
     );
+  }
+
+  Widget buildForm() {
+    return Obx(() {
+      var isLoading = controller.isLoading.value;
+
+      return Form(
+        key: controller.formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              enabled: !isLoading,
+              controller: controller.email,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                label: Text(t.email),
+                hintText: t.email_hint,
+                prefixIcon: Icon(Icons.email_outlined),
+              ),
+              validator: (value) {
+                if (value == null || !isEmail(value)) {
+                  return t.email_invalid;
+                }
+                return null;
+              },
+            ),
+            Padding(padding: EdgeInsets.only(top: 8)),
+            SizedBox(
+              width: double.infinity,
+              child: primaryButton(
+                loading: isLoading,
+                onPressed: controller.submit,
+                label: t.forgot_password_submit,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
